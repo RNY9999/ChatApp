373a46df3e3d3f964c599986d13dde3f1ca24c37
renya-hisamatsu <renya.hisamatsu@g.dreamcareer.co.jp>
2025-05-28 00:31:38 +0900

    feat #14: userの参照先をPostgreSQLに変更中
    現在エラー発生中

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,16 +1,37 @@
 import { Router } from 'express';
+import prisma from '../prisma';
 import User from '../models/user';
 
 const router = Router();
 
 
-// ユーザー一覧の取得
-router.get('/getList', async (req, res) => {
+// ユーザー一覧の取得 MongoDB
+
+// router.get('/getList', async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     res.status(200).json(users);
+//   } catch (err: any) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+// ユーザー一覧の取得 PostgreSQL
+router.get('/getList', async (req, res): Promise<void> => {
   try {
-    const users = await User.find();
-    res.status(200).json(users);
+      const users = await prisma.user.findMany({
+          select: {
+              id: true,
+              username: true,
+              password: true,
+              deleted: true,
+              createdAt: true,
+              updatedAt: true,
+          }
+      });
+      res.status(200).json(users);
   } catch (err: any) {
-    res.status(500).json({ error: err.message });
+      res.status(500).json({ error: err.message });
   }
 });
 
