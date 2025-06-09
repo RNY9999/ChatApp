c65134e392e72868130c37f724196a4b21ef87d5
renya-hisamatsu <renya.hisamatsu@g.dreamcareer.co.jp>
2025-06-10 01:25:05 +0900

    feature: toノートPC

--- admin/src/app/users/edit/[id]/editForm.tsx
+++ admin/src/app/users/edit/[id]/editForm.tsx
@@ -1,23 +1,74 @@
 'use client';
 import axios from '@utils/axiosInstance';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { User } from '@projectTypes/index';
+import validationSchema from '@utils/validationSchema';
+import { RegisterUser } from '@projectTypes/index'
+import { useForm, SubmitHandler } from 'react-hook-form';
+
 
 const EditForm = ({user}: {user: User}) => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm<RegisterUser>({
+        mode: 'onChange',
+        resolver: zodResolver(validationSchema),
+        defaultValues: {
+            username: user.username,
+            password: user.password,
+        }
+    })
+
+    const onSubmit: SubmitHandler<RegisterUser> = async (data) => {
+        console.log('update user info');
+        console.log(data);
+        console.log('==========');
+
+        try {
+            // axiosでの処理
+            console.log('updated!');
+        } catch (error) {
+            console.error('catch error in update user info:', error);
+        }
+    };
     return (
-        <form action="" className='edit-form'>
+        <form action="" onSubmit={handleSubmit(onSubmit)} className='edit-form'>
             <div className="edit-form__form">
-                <label htmlFor="username">username</label>
-                <input type="text" name='username' id='username' defaultValue={ user.username}/>
-                <p className="supporting-text">
-                    ※文字数は5文字
+                <label htmlFor="username" className='edit-form__label'>ユーザー名</label>
+                <input type="text" id='username' defaultValue={ watch('username')} className='edit-form__input-area' { ...register('username') }/>
+                { errors.username?.message && 
+                <p className="edit-form__error-text">
+                    { errors.username.message}
                 </p>
+                }
             </div>
-            <label htmlFor="password">password : </label>
-            <input type="text" name='password' defaultValue={ user.password}/>
-            <label htmlFor="deleted">アカウントのステータス : </label>
-            <input type="radio" name="deleted" value={"停止"} defaultChecked={ user.deleted}/>
-            <input type="radio" name="deleted" value={"アクティブ"} defaultChecked={ !user.deleted}/>
-            <input type="button" value="確認" />
+            <div className="edit-form__form">
+                <label htmlFor="password" className='edit-form__label'>パスワード</label>
+                <input type="text" id='password' defaultValue={ watch('password')} className='edit-form__input-area' { ...register('password') }/>
+                { errors.password?.message && 
+                <p className="edit-form__error-text">
+                    { errors.password.message}
+                </p>
+                }
+            </div>
+            <fieldset className="edit-form__form">
+                <legend className='edit-form__legend'>アカウントのステータス</legend>
+                <label htmlFor='isDeleted' className='edit-form__radio-label'>
+                    <input type="radio" name="deleted" id='isDeleted' value={"停止"} defaultChecked={ user.deleted} className='edit-form__radio-area'/>
+                    <span className='edit-form__custom-radio'></span>
+                    <p>停止</p>
+                </label>
+                <label htmlFor='isActive' className='edit-form__radio-label'>
+                    <input type="radio" name="deleted" id='isActive' value={"アクティブ"} defaultChecked={ !user.deleted} className='edit-form__radio-area --none'/>
+                    <span className='edit-form__custom-radio'></span>
+                    <p>アクティブ</p>
+                </label>
+            </fieldset>
+                <input type="submit" value="確認" className='edit-form__submit' />
         </form>
     );
 }
--- admin/src/types/index.ts
+++ admin/src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './user';
 export * from './params';
+export * from './registerUser';
