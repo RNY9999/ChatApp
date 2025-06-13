49f6e1409bc309e5c9b43ae9f8a4e8bdd4ba66f5
RNY9999 <84605260+RNY9999@users.noreply.github.com>
2025-06-13 21:18:27 +0900

    chore: loosen tsconfig for offline build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import app from './app';
 import dotenv from 'dotenv';
 
-// .envファイルの読み込み
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-console.log(process.env.MESSAGE);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
